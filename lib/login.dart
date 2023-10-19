1458b98cfcb405df713267cca5b4f64948b7c3f3
DoniG09 <81458410+DoniG09@users.noreply.github.com>
2023-10-18 21:21:34 -0600

    Modificaciones de las pantallas de: Signup, Tickets, Login, Editar Perfil para que no hayan overflows al momento de ingresar textos, adicionalmente de arreglar errores en los inputs ingresados

--- lib/login.dart
+++ lib/login.dart
@@ -10,220 +10,228 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-
-      resizeToAvoidBottomInset: false, //PARA QUE CUANDO SE ABRA EL TECLADO NO EXISTA OVERFLOW
-      
-      backgroundColor: Colors.white,
-
-      appBar: AppBar(
-
-        elevation: 0,
-        systemOverlayStyle: SystemUiOverlayStyle(statusBarBrightness: Brightness.light),
-        backgroundColor: Colors.white,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if(!currentFocus.hasPrimaryFocus){
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+    
+        resizeToAvoidBottomInset: false, //PARA QUE CUANDO SE ABRA EL TECLADO NO EXISTA OVERFLOW
         
-        leading: IconButton(
-          onPressed: () {
-
-            Navigator.pop(
-              context
-            );
-            
-          },
-          icon: Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
+        backgroundColor: Colors.white,
+    
+        appBar: AppBar(
+    
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle(statusBarBrightness: Brightness.light),
+          backgroundColor: Colors.white,
+          
+          leading: IconButton(
+            onPressed: () {
+    
+              Navigator.pop(
+                context
+              );
+              
+            },
+            icon: Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
+          ),
+    
         ),
-
-      ),
-      
-      body: Container(
-
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-          children: <Widget>[
-
-            Expanded( //PARA QUE CUBRA TODA LA PANTALLA
-
-              child: Column(
-            
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            
-                children: <Widget>[
-            
-                  Column(
-            
-                    children: <Widget>[
-            
-                      Text("Login", style: TextStyle(
-            
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold
-            
-                      ),),
-            
-                      SizedBox(height: 20,),
-            
-                      Text("Accede a tu cuenta", style: TextStyle(
-            
-                        fontSize: 15,
-                        color: Colors.grey[700]
-            
-                      ),),
-            
-                    ],
-            
-                  ),
-            
-                  //AQUÍ
-            
-                  Padding(
-            
-                    padding: EdgeInsets.symmetric(horizontal: 40),
-            
-                    child: Column(
-            
+        
+        body: Container(
+    
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+    
+          child: Column(
+    
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    
+            children: <Widget>[
+    
+              Expanded( //PARA QUE CUBRA TODA LA PANTALLA
+    
+                child: Column(
+              
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              
+                  children: <Widget>[
+              
+                    Column(
+              
                       children: <Widget>[
-            
-                        makeInput(label: "Correo"),
-                        makeInput(label: "Contraseña", obsecureText: true),
-                        //makeInput(label: "Confirma tu Contraseña", obsecureText: true),
-            
-                      ],
-            
-                    ),
-            
-                  ),
-            
-                  //AQUÍ
-            
-                  Padding(
-                    
-                    padding: EdgeInsets.symmetric(horizontal: 40),
-            
-                    //AQUI
-                    child: Container(
-            
-                      padding: EdgeInsets.only(top: 1.5, left: 1.5),
-                      
-                      decoration: BoxDecoration(
-            
-                        borderRadius: BorderRadius.circular(50),
-            
-                        border: Border(
-            
-                          bottom: BorderSide(color: Colors.black),
-                          top: BorderSide(color: Colors.black),
-                          left: BorderSide(color: Colors.black),
-                          right: BorderSide(color: Colors.black),
-            
-                        )
-            
-                      ),
-                      
-                      child: MaterialButton(
-            
-                        minWidth: double.infinity,
-                        height: 60,
-
-                        onPressed: (){
-
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => Dashboard()));
-
-                        },
-
-                        color: Color.fromARGB(255, 81, 124, 193),
-                        elevation: 0,
-            
-                        shape: RoundedRectangleBorder(
-            
-                          borderRadius: BorderRadius.circular(50)
-            
-                        ),
-            
-                        child: Text(
-            
-                          "Ingresar", 
-            
-                          style: TextStyle(
-                            
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18
-            
-                          ),
-            
-                        ),
-            
-                      ),
-            
-                    ),
-                  
-                  ),
-            
-                  //AQUÍ
-            
-                  Row(
-            
-                    mainAxisAlignment: MainAxisAlignment.center,
-            
-                    children: <Widget>[
-            
-                      Text("¿No tienes una cuenta?"),
-
-                      GestureDetector(
-
-                        child: Text(" Regístrate aquí", style: TextStyle(
-                  
-                          fontWeight: FontWeight.w600, fontSize:  18
-
+              
+                        Text("Login", style: TextStyle(
+              
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold
+              
                         ),),
+              
+                        SizedBox(height: 20,),
+              
+                        Text("Accede a tu cuenta", style: TextStyle(
+              
+                          fontSize: 15,
+                          color: Colors.grey[700]
+              
+                        ),),
+              
+                      ],
+              
+                    ),
+              
+                    //AQUÍ
+              
+                    Padding(
+              
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+              
+                      child: Column(
+              
+                        children: <Widget>[
+              
+                          makeInput(label: "Correo"),
+                          makeInput(label: "Contraseña", obsecureText: true),
+                          //makeInput(label: "Confirma tu Contraseña", obsecureText: true),
+              
+                        ],
+              
+                      ),
+              
+                    ),
+              
+                    //AQUÍ
+              
+                    Padding(
+                      
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+              
+                      //AQUI
+                      child: Container(
+              
+                        padding: EdgeInsets.only(top: 1.5, left: 1.5),
                         
-                        onTap: (){
-
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
-
-                        },
-
-                      )
-            
-                    ],
-            
+                        decoration: BoxDecoration(
+              
+                          borderRadius: BorderRadius.circular(50),
+              
+                          border: Border(
+              
+                            bottom: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black),
+                            right: BorderSide(color: Colors.black),
+              
+                          )
+              
+                        ),
+                        
+                        child: MaterialButton(
+              
+                          minWidth: double.infinity,
+                          height: 60,
+    
+                          onPressed: (){
+    
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => Dashboard()));
+    
+                          },
+    
+                          color: Color.fromARGB(255, 81, 124, 193),
+                          elevation: 0,
+              
+                          shape: RoundedRectangleBorder(
+              
+                            borderRadius: BorderRadius.circular(50)
+              
+                          ),
+              
+                          child: Text(
+              
+                            "Ingresar", 
+              
+                            style: TextStyle(
+                              
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18
+              
+                            ),
+              
+                          ),
+              
+                        ),
+              
+                      ),
+                    
+                    ),
+              
+                    //AQUÍ
+              
+                    Row(
+              
+                      mainAxisAlignment: MainAxisAlignment.center,
+              
+                      children: <Widget>[
+              
+                        Text("¿No tienes una cuenta?"),
+    
+                        GestureDetector(
+    
+                          child: Text(" Regístrate aquí", style: TextStyle(
+                    
+                            fontWeight: FontWeight.w600, fontSize:  18
+    
+                          ),),
+                          
+                          onTap: (){
+    
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
+    
+                          },
+    
+                        )
+              
+                      ],
+              
+                    )
+              
+                  ],
+              
+                ),
+              ),
+    
+              //AQUÍ
+    
+              Container(
+    
+                height: MediaQuery.of(context).size.height / 3,
+    
+                decoration: BoxDecoration(
+    
+                  image: DecorationImage(
+    
+                    image: AssetImage("assets/images/Background.png"),
+                    fit: BoxFit.cover
+    
                   )
-            
-                ],
-            
-              ),
-            ),
-
-            //AQUÍ
-
-            Container(
-
-              height: MediaQuery.of(context).size.height / 3,
-
-              decoration: BoxDecoration(
-
-                image: DecorationImage(
-
-                  image: AssetImage("assets/images/Background.png"),
-                  fit: BoxFit.cover
-
-                )
-
-              ),
-
-            )
-
-          ],
-
+    
+                ),
+    
+              )
+    
+            ],
+    
+          ),
+    
         ),
-
+    
       ),
-
     );
 
   }
--- lib/widgets/tickets.dart
+++ lib/widgets/tickets.dart
@@ -12,168 +12,180 @@ class TicketsPage extends StatefulWidget {
 class _TicketsPage extends State<TicketsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      /*appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 81, 124, 193),
-        title: Text.rich(
-          TextSpan(
-              text: 'Tickets',
-              style: GoogleFonts.josefinSans(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white)),
-          textAlign: TextAlign.center,
-        ),
-      ),*/
-      body: Padding(
-        padding: const EdgeInsets.all(0),
-        child: Column(
-          children: [
-            const Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if(!currentFocus.hasPrimaryFocus){
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        /*appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 81, 124, 193),
+          title: Text.rich(
+            TextSpan(
+                text: 'Tickets',
+                style: GoogleFonts.josefinSans(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white)),
+            textAlign: TextAlign.center,
+          ),
+        ),*/
+        body: SingleChildScrollView(
+          reverse: true,
+          physics: BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(0),
+            child: Column(
+              children: [
+                const Row(
+                    //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                const SizedBox(height: 8),
+                Text(
+                  "Tickets".toUpperCase(),
+                  style: GoogleFonts.croissantOne(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 255, 255, 255)),
                 ),
-            const SizedBox(height: 8),
-            Text(
-              "Tickets".toUpperCase(),
-              style: GoogleFonts.croissantOne(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: const Color.fromARGB(255, 255, 255, 255)),
-            ),
-            Text(
-              "\nCrear un ticket para el \n departamento de recursos humanos",
-              style: GoogleFonts.croissantOne(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  color: const Color.fromARGB(255, 255, 255, 255)),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 10),
-            const TicketWidget(
-              width: 300,
-              height: 500,
-              isCornerRounded: true,
-              child: Stack(
-                children: [
-                  Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Center(
-                        child: Column(
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.blue,
-                              radius: 60,
-                              child: CircleAvatar(
-                                backgroundImage:
-                                    AssetImage('assets/images/user.png'),
-                                backgroundColor: Color.fromARGB(255, 172, 172, 172),
-                                radius: 55,
-                              ),
-                            ),
-                            Text(
-                              "Anthony Avila",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w700,
-                                  fontStyle: FontStyle.italic),
-                            ),
-                            SizedBox(height: 10),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                Text(
+                  "\nCrear un ticket para el \n departamento de recursos humanos",
+                  style: GoogleFonts.croissantOne(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 255, 255, 255)),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 10),
+                const TicketWidget(
+                  width: 300,
+                  height: 500,
+                  isCornerRounded: true,
+                  child: Stack(
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.all(20),
+                          child: Center(
+                            child: Column(
                               children: [
-                                Icon(Icons.archive,
-                                    color: Colors.blueGrey, size: 18),
+                                CircleAvatar(
+                                  backgroundColor: Colors.blue,
+                                  radius: 60,
+                                  child: CircleAvatar(
+                                    backgroundImage:
+                                        AssetImage('assets/images/user.png'),
+                                    backgroundColor: Color.fromARGB(255, 172, 172, 172),
+                                    radius: 55,
+                                  ),
+                                ),
                                 Text(
-                                  'Dep. Contabilidad',
+                                  "Anthony Avila",
                                   style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
                                       fontWeight: FontWeight.w700,
-                                      color: Colors.blueGrey),
+                                      fontStyle: FontStyle.italic),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.archive,
+                                        color: Colors.blueGrey, size: 18),
+                                    Text(
+                                      'Dep. Contabilidad',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          color: Colors.blueGrey),
+                                    )
+                                  ],
+                                ),
+                                TextField(
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Asunto',
+                                      hintStyle: TextStyle(color: Colors.grey)),
+                                ),
+                                SizedBox(height: 20),
+                                TextField(
+                                  decoration: InputDecoration(
+                                      contentPadding:
+                                          EdgeInsets.symmetric(vertical: 90),
+                                      border: InputBorder.none,
+                                      hintText: 'Contexto',
+                                      hintStyle: TextStyle(color: Colors.grey)),
                                 )
                               ],
                             ),
-                            TextField(
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Asunto',
-                                  hintStyle: TextStyle(color: Colors.grey)),
-                            ),
-                            SizedBox(height: 20),
-                            TextField(
-                              decoration: InputDecoration(
-                                  contentPadding:
-                                      EdgeInsets.symmetric(vertical: 90),
-                                  border: InputBorder.none,
-                                  hintText: 'Contexto',
-                                  hintStyle: TextStyle(color: Colors.grey)),
-                            )
-                          ],
-                        ),
-                      ))
-                ],
-              ),
+                          ))
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 40,
+                  width: 140,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 139, 194, 68)),
+                    child: const Text('Enviar'),
+                  ),
+                )
+              ],
             ),
-            const SizedBox(height: 20),
-            SizedBox(
-              height: 40,
-              width: 140,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 139, 194, 68)),
-                child: const Text('Enviar'),
-              ),
-            )
-          ],
+          ),
         ),
+        /*bottomNavigationBar: GNav(
+          backgroundColor: const Color.fromARGB(255, 81, 124, 193),
+          color: Colors.white,
+          activeColor: const Color.fromARGB(255, 255, 255, 255),
+          tabBackgroundColor: const Color.fromARGB(255, 115, 150, 207),
+          gap: 7,
+          tabs: [
+            GButton(
+                icon: Icons.home,
+                text: 'Inicio',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Dashboard()),
+                  );
+                }),
+            GButton(
+                icon: Icons.airplane_ticket_rounded,
+                text: 'Ticket',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const TicketsPage()),
+                  );
+                }),
+            GButton(
+                icon: Icons.picture_as_pdf_rounded,
+                text: 'Documentos',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Documentos()),
+                  );
+                }),
+            GButton(
+                icon: Icons.person,
+                text: 'Perfil',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PerfilUsuario()),
+                  );
+                }),
+          ],
+        ),*/
       ),
-      /*bottomNavigationBar: GNav(
-        backgroundColor: const Color.fromARGB(255, 81, 124, 193),
-        color: Colors.white,
-        activeColor: const Color.fromARGB(255, 255, 255, 255),
-        tabBackgroundColor: const Color.fromARGB(255, 115, 150, 207),
-        gap: 7,
-        tabs: [
-          GButton(
-              icon: Icons.home,
-              text: 'Inicio',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Dashboard()),
-                );
-              }),
-          GButton(
-              icon: Icons.airplane_ticket_rounded,
-              text: 'Ticket',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const TicketsPage()),
-                );
-              }),
-          GButton(
-              icon: Icons.picture_as_pdf_rounded,
-              text: 'Documentos',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Documentos()),
-                );
-              }),
-          GButton(
-              icon: Icons.person,
-              text: 'Perfil',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const PerfilUsuario()),
-                );
-              }),
-        ],
-      ),*/
     );
   }
 }
